af8164827d7261ce8ff8148e45a085468499c03e
Anderson Santos <anderson.santos.silva@live.com>
2021-03-06 21:39:25 -0300

    checkbox estilizado

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <main class="wrapper">
+        <section class="checkbox-wrapper">
+            
+                
+                    <input type="checkbox" name="linguagens" class="checkbox" id="ling01" value="c#"> 
+                    <label for="ling01" class="checkbox-label">C#</label>
+                    <input type="checkbox" name="linguagens"  class="checkbox"id="ling02" value="php" checked>
+                    <label for="ling02" class="checkbox-label">Php</label>
+                    <input type="checkbox" name="linguagens"  class="checkbox" id="ling03" value="js">
+                    <label for="ling03" class="checkbox-label">Js</label>
+                    <input type="checkbox" name="linguagens"  class="checkbox" id="ling04" value="java">
+                    <label for="ling04" class="checkbox-label">Java</label>
+            
+        </section>
+    </main>
+
+</body>
+
+</html>
